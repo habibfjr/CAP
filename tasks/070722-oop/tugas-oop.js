2a320557bb63204cdb25ab2c2ab8ba1074392f11
habibfjr <habibf99@gmail.com>
2022-07-08 21:22:26 +0700

    updated and finished tugas OOP

--- tasks/070722-oop/tugas-oop.js
+++ tasks/070722-oop/tugas-oop.js
@@ -10,32 +10,27 @@ class Supermarket {
     }
 
     cekStock(){
-        
+
         for (let i=0;i<this.item.length;i++){
-         if(this.item[i][0] === this.namaKopi){
-            console.log(this.item[i][0])
-            if(this.item[i][1] < this.jumlahKopi){
-                console.log(this.item[i][1]) 
+
+         if(this.item[i][0] === this.namaKopi &&
+            (this.item[i][1] < this.jumlahKopi)){
+                                
+                return `Pesan item sebanyak = ${this.item[i][1] + this.item[i][1]*2}`
+
+            } else if (this.item[i][0] === this.namaKopi &&
+                (this.item[i][1] >= this.jumlahKopi)){
+
+                    return `Stok ${this.item[i][0]} tersisa = ${this.item[i][1] - this.jumlahKopi}`
+
+            } else{
+
+                return ("Item tidak tersedia")
                 
-                return `jumlah barang = ${this.item[i][1]}` 
             }
          }   
 }
     }
 
-    kurangiStock(){
-        for(let i=0;i<this.item.length;i++){
-            if(this.item){
-            let x = this.item[i][1]-this.jumlahKopi;
-        }
-        }
-        return `sisa barang =${x}`
-    }
-
-    stockEmpty(){
-        
-    }
-}
-
-let cafe = new Supermarket ('Arabica', 30)
+let cafe = new Supermarket ('Arabica', 20)
 console.log(cafe.cekStock())
